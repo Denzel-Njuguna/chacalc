a78920ca53e1a552f7002261a4ac22d8acb6fcdd
Mark <markmoreka@gmail.com>
2025-12-16 10:00:00 +0300

    Finished Auth

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import (
-	"chacalc/internal/auth"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
+	"chacalc/internal/auth"
 	"chacalc/internal/config"
 	"chacalc/internal/health"
+	authMiddleware "chacalc/internal/middleware"
 )
 
 func New(cfg *config.Config) http.Handler {
@@ -23,6 +24,12 @@ func New(cfg *config.Config) http.Handler {
 
 	r.Route("/auth", func(r chi.Router) {
 		r.Post("/signup", authHandler.Signup)
+		r.Post("/login", authHandler.Login)
+		r.Post("/logout", authHandler.Logout)
+		r.Post("/forgot-password", authHandler.ForgotPassword)
+		r.With(authMiddleware.Auth(cfg)).Post("/reset-password", authHandler.ResetPassword)
+		r.With(authMiddleware.Auth(cfg)).Post("/update-password", authHandler.UpdatePassword)
+		r.With(authMiddleware.Auth(cfg)).Get("/me", authHandler.Me)
 	})
 
 	return r
